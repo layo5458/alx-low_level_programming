3836e8f62e6521098da6f7767812025c81114cf3
layo5458 <punmilayoigbasan@gmail.com>
2022-03-12 11:51:30 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,6 @@
  *    *Return:0                                                                                                   
  *
  *     */                                                                                                          
-
 int main(void)                                                                                               
 
 {                 printf("with proper grammar, but the outcome is a piece of art,\n");
