86ad39b85166f47501c4bf65ba0dd05c400d7bc6
layo5458 <punmilayoigbasan@gmail.com>
2022-03-12 11:20:45 -0800

    Use the function printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-nclude<stdio.h>                                                                                            
+#include<stdio.h>                                                                                            
 
 /**                                                                                                          
  *
@@ -12,9 +12,6 @@ nclude<stdio.h>
 
 int main(void)                                                                                               
 
-{                                                                                                     
-	        printf("\"with proper grammar, but the outcome is a piece of art,\n");                                        
-
-		return (0);                                                                                                  
-
-}              
+{                 printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
