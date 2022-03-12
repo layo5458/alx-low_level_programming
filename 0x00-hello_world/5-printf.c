44d1996e5476df554e572e465632e598291c41c6
layo5458 <punmilayoigbasan@gmail.com>
2022-03-12 12:40:13 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
-#include<stdio.h>                                                                                            
-
-/**                                                                                                          
+#include<stdio.h>
+/**
+ * main - Entry point
  *
- *  *main - Entry point                                                                                         
- *
- *   *                                                                                                           
- *
- *    *Return:0                                                                                                   
- *
- *     */                                                                                                          
-int main(void)                                                                                               
-
-{                 printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
 }
+
